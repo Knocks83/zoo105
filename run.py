3ebb30ca5f8a364dcb3f500414e6fffb127b181e
Luca Fenu <luca.fenu01@gmail.com>
2022-01-10 18:49:36 +0100

    Splitted video and audio download

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from requests import get
+from requests import get, head
 from bs4 import BeautifulSoup
 
 from datetime import datetime
 from locale import setlocale, LC_ALL
 
-from os import getenv
-from os.path import dirname, realpath
+from os import getenv, remove
+from os.path import dirname, realpath, exists
 from dotenv import load_dotenv
 import argparse
 import logging
@@ -14,7 +14,7 @@ import toolbox
 
 # Set basic vars
 websiteURL = "https://zoo.105.net/"
-audioBaseURL = 'http://ms-pod.mediaset.net/repliche//{year:04d}/{month:02d}/{day:02d}/{daytext:s}_{day:02d}{month:02d}{year:04d}_zoo.mp3'
+audioBaseURL = 'http://ms-pod.mediaset.net/repliche//{year:04d}/{month:d}/{day:02d}/{daytext:s}_{day:02d}{month:02d}{year:04d}_zoo.mp3'
 
 logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser()
@@ -34,46 +34,58 @@ if(not (arguments.doSendAudio or arguments.doSendVideo)):
 load_dotenv(dotenv_path=dirname(realpath(__file__))+'/config.env')
 setlocale(LC_ALL, "it_IT.UTF8")
 
-# Get and objectify the page
-page = get(websiteURL)
-soup = BeautifulSoup(page.content, "html.parser")
-
-videos = []
-episodeTitles = []
-
-# Find the div that contains the videos
-videoBox = soup.find(class_="cont_visualizzazione")
-
-# Loop through the divs with class box
-for episode in videoBox.findAll(class_="box"):
-    # Find the webpage of the video and the title
-    episodeURL = episode.find("a")['href']
-    episodeTitles.append(episode.find("a").decode_contents())
-
-    # Find the media url from inside the video webpage
-    guid = toolbox.findGUID(episodeURL)
-    videoURL = toolbox.findDownloadURL(guid)
-    videos.append(videoURL)
-
-# split the date in different variables to check the date of the last media
-day, month, year = episodeTitles[0].split(' ')[-1].split('-')
 today = datetime.today()
+filename = "{daytext:s}_{day:02d}{month:02d}{year:04d}_zoo".format(
+    day=today.day, month=today.month, year=today.year, daytext=today.strftime("%a").lower())
 
-if (day == str(today.day) and month == str(today.month) and year == str(today.year)):
-    telegram = toolbox.Telegram(getenv('TELEGRAM_API_TOKEN'), getenv(
-        'TELEGRAM_CHAT_ID'), getenv('TELEGRAM_API_URL'))
+telegram = toolbox.Telegram(getenv('TELEGRAM_API_TOKEN'), getenv(
+    'TELEGRAM_CHAT_ID'), getenv('TELEGRAM_API_URL'))
+
+if (arguments.doSendAudio):
     audioURL = audioBaseURL.format(
         day=today.day, month=today.month, year=today.year, daytext=today.strftime("%a").lower())
-
-    filename = "{daytext:s}_{day:02d}{month:02d}{year:04d}_zoo".format(
-        day=today.day, month=today.month, year=today.year, daytext=today.strftime("%a").lower())
-
-    if (arguments.doSendAudio):
+    
+    if (head(audioURL).status_code in [200, 302]):
         toolbox.download(audioURL, filename+'.mp3')
         r = telegram.sendAudio(filename+'.mp3')
         logging.debug(r)
 
-    if (arguments.doSendVideo):
+        if exists(filename+'.mp3'):
+            remove(filename+'.mp3')
+    else:
+        logging.error('Audio not found!')
+
+if (arguments.doSendVideo):
+    # Get and objectify the page
+    page = get(websiteURL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    videos = []
+    episodeTitles = []
+
+    # Find the div that contains the videos
+    videoBox = soup.find(class_="cont_visualizzazione")
+
+    # Loop through the divs with class box
+    for episode in videoBox.findAll(class_="box"):
+        # Find the webpage of the video and the title
+        episodeURL = episode.find("a")['href']
+        episodeTitles.append(episode.find("a").decode_contents())
+
+        # Find the media url from inside the video webpage
+        guid = toolbox.findGUID(episodeURL)
+        videoURL = toolbox.findDownloadURL(guid)
+        videos.append(videoURL)
+
+    # split the date in different variables to check the date of the last media
+    day, month, year = episodeTitles[0].split(' ')[-1].split('-')
+
+    if (day == str(today.day) and month == str(today.month) and year == str(today.year)):
         toolbox.download(videos[0], filename+'.mp4')
         r = telegram.sendVideo(filename+'.mp4')
         logging.debug(r)
+
+        if exists(filename+'.mp4'):
+            remove(filename+'.mp4')
+    else:
+        logging.error('Video not found!')
