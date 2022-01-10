ce951a9bc4d50d414a2af651b872e622fb53d074
Luca Fenu <luca.fenu01@gmail.com>
2022-01-10 18:18:47 +0100

    Added CLI parameters

--- run.py
+++ run.py
@@ -7,13 +7,29 @@ from locale import setlocale, LC_ALL
 from os import getenv
 from os.path import dirname, realpath
 from dotenv import load_dotenv
-import toolbox
+import argparse
 import logging
+import toolbox
 
-logging.basicConfig(level=logging.DEBUG)
+
 # Set basic vars
 websiteURL = "https://zoo.105.net/"
 audioBaseURL = 'http://ms-pod.mediaset.net/repliche//{year:04d}/{month:02d}/{day:02d}/{daytext:s}_{day:02d}{month:02d}{year:04d}_zoo.mp3'
+
+logging.basicConfig(level=logging.DEBUG)
+parser = argparse.ArgumentParser()
+
+# Parse CLI arguments
+parser.add_argument('--audio', help='Send audio file',
+                    dest='doSendAudio', default=False, action='store_true')
+parser.add_argument('--video', help='Send video file',
+                    dest='doSendVideo', default=False, action='store_true')
+arguments = parser.parse_args()
+
+if(not (arguments['doSendAudio'] and arguments['doSendVideo'])):
+    logging.info('You didn\'t specify any action, run run.py -h')
+    exit(0)
+
 
 load_dotenv(dotenv_path=dirname(realpath(__file__))+'/config.env')
 setlocale(LC_ALL, "it_IT.UTF8")
@@ -51,12 +67,13 @@ if (day == str(today.day) and month == str(today.month) and year == str(today.ye
 
     filename = "{daytext:s}_{day:02d}{month:02d}{year:04d}_zoo".format(
         day=today.day, month=today.month, year=today.year, daytext=today.strftime("%a").lower())
-    
-    
-    toolbox.download(audioURL, filename+'.mp3')
-    r = telegram.sendAudio(filename+'.mp3')
-    logging.debug(r)
 
-    toolbox.download(videos[0], filename+'.mp4')
-    r = telegram.sendVideo(filename+'.mp4')
-    logging.debug(r)
+    if (arguments['doSendAudio']):
+        toolbox.download(audioURL, filename+'.mp3')
+        r = telegram.sendAudio(filename+'.mp3')
+        logging.debug(r)
+
+    if (arguments['doSendVideo']):
+        toolbox.download(videos[0], filename+'.mp4')
+        r = telegram.sendVideo(filename+'.mp4')
+        logging.debug(r)
